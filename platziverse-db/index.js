ec0cc0e8e73f3d1868751731a40ae0071e4f7645
Samuel Paez <spaez@viva.com.do>
2022-08-02 22:38:08 -0400

    terminado conexion y creacion de tablas

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,20 +1,21 @@
 'use strict'
 const setupDatabase = require('./lib/db')
-const setupAgentModel = requiere('./models/agent')
-const setupMetricModel = requiere('./models/metric')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
 
 module.exports = async function (config) {
-  
   const sequelize = setupDatabase(config)
-  const AgentModel = setupAgentModel(config);
-  const MetricModel = setupMetricModel(config);
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
 
   AgentModel.hasMany(MetricModel)
   MetricModel.belongsTo(AgentModel)
 
   await sequelize.authenticate()
 
-  
+  if (config.setup) {
+    await sequelize.sync({ force: true })
+  }
   const Agent = {}
   const Metric = {}
 
