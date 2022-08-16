c73c51e0f92de655a51f91dd2dee03514bb11480
Samuel Paez <spaez@viva.com.do>
2022-08-15 23:39:51 -0400

    Completando modulo de platziverse agent

--- platziverse-agent/README.md
+++ platziverse-agent/README.md
@@ -6,10 +6,36 @@
     const PlatziverseAgent = require('platziverse-agent')
 
     const agent = new PlatziverseAgent({
-        interval:2000
+        interval:2000,
+        name:'myapp',
+        username: 'admin'
     })
 
+    agent.addMetric('rss', function getRss () {
+        return process.memoryUsage().rss
+    })
+
+    agent.addMetric('rss', function getRandomPromise () {
+        return Promise.resolve(Math.random())
+    })
+
+    agent.addMetric('callbackMetric', function getRandomCallback (callback) {
+        setTimeout(() => {
+          callback(null, Math.random())
+        }, 1000)
+    })
     agent.connect()
+
+    // This agent only
+    agent.on('connect', handle)
+    agent.on('disconnect', handle)
+    agent.on('message', handle)
+
+    // Other agent sending me (this not me , broadcast)
+    agent.on('agent/connect')
+    agent.on('agent/disconnect')
+    agent.on('agent/message')
+
 
     agent.on('agent/message', payload => {
         console.log(payload)
--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,23 +1,108 @@
 'use strict'
 
+const debug = require('debug')('platziverse:agent')
+const os = require('os')
+const util = require('util')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
 const EventEmmiter = require('events')
+const { parsePayload } = require('platziverse-utils')
+const uuid = require('uuid')
+
+const options = {
+  name: 'untitled',
+  username: 'unnamed',
+  interval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
+  }
+}
 
 class PlaziverseAgent extends EventEmmiter {
   constructor (opts) {
     super()
-    this._options = opts
+    this._options = defaults(opts, options)
     this._started = false
     this._timer = null
+    this._client = null
+    this._agentId = null
+    this._metrics = new Map()
+  }
+
+  addMetric (type, func) {
+    this._metrics.set(type, func)
+  }
+
+  removeMetric (type) {
+    this._metrics.delete(type)
   }
 
   connect () {
     if (!this._started) {
-      this._started = true
-      this.emit('connected')
       const opts = this._options
-      this._timer = setInterval(() => {
-        this.emit('agent/message', 'this is a message')
-      }, opts.interval)
+
+      this._client = mqtt.connect(opts.host)
+      this._started = true
+      this._client.subscribe('agent/message')
+      this._client.subscribe('agent/connected')
+      this._client.subscribe('agent/disconnected')
+
+      this._client.on('connect', () => {
+        this._agentId = uuid.v4()
+
+        this.emit('connected', this._agentId)
+
+        this._timer = setInterval(async () => {
+          if (this._metrics.size > 0) {
+            const message = {
+              agent: {
+                uuid: this._agentId,
+                username: opts.username,
+                name: opts.name,
+                host: os.hostname() || 'localhost',
+                pid: process.pid
+              },
+              metrics: [],
+              timestamp: new Date().getTime()
+            }
+
+            for (let [metric, func] of this._metrics) {
+              if (func.length === 1) {
+                func = util.promisify(func)
+              }
+
+              message.metrics.push({
+                type: metric,
+                value: await Promise.resolve(func())
+              })
+            }
+
+            debug('Sending', message)
+
+            this._client.publish('agent/message', JSON.stringify(message))
+            this.emit('message', message)
+          }
+          this.emit('agent/message', 'this is a message')
+        }, opts.interval)
+      })
+
+      this._client.on('message', (topic, payload) => {
+        payload = parsePayload(payload)
+        let broadcast = false
+        switch (topic) {
+          case 'agent/connected':
+          case 'agent/disconnected':
+          case 'agent/message':
+            broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
+            break
+        }
+
+        if (broadcast) {
+          this.emit(topic, payload)
+        }
+      })
+
+      this._client.on('error', () => this.disconnect())
     }
   }
 
@@ -25,7 +110,8 @@ class PlaziverseAgent extends EventEmmiter {
     if (this._started) {
       clearInterval(this._timer)
       this._started = false
-      this.emit('disconnected')
+      this.emit('disconnected', this._agentId)
+      this._client.end()
     }
   }
 }
