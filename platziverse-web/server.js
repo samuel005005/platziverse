98b19a20fd9b8a73a45e5ae593bc936e5cbebbd5
Samuel Paez <spaez@viva.com.do>
2022-09-26 07:14:43 -0400

    distribuir eventos del agente al socket.io

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const debug = require('debug')('platziverse:web')
-const { handle } = require('platziverse-utils')
+const { handle, utils } = require('platziverse-utils')
 const { handleFatalError } = handle
+const { pipe } = utils
 const http = require('http')
 const path = require('path')
 const express = require('express')
 const socketio = require('socket.io')
 const chalk = require('chalk')
 const PlatziverseAgent = require('platziverse-agent')
-
 
 const port = process.env.PORT || 8080
 const app = express()
@@ -24,30 +24,14 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Socket.io / Websockets
 
 io.on('connect', socket => {
-
   debug(`Connected ${socket.id}`)
-  // socket.on('agent/message', payload => {
-  //   console.log(payload)
+
+  // agent.on('agent/disconnected', payload => {
+  //   socket.emit('agent/disconnected', payload)
   // })
 
-  // setInterval(() => {
-  //   socket.emit('agent/message', { agent: 'xxx-yyy'})
-  // }, 2000)
-
-  agent.on('agent/message', payload => {
-      socket.emit('agent/message', payload)
-  })
-
-  agent.on('agent/connected', payload => {
-    socket.emit('agent/connected', payload)
-  })
-  
-  agent.on('agent/disconnected', payload => {
-    socket.emit('agent/disconnected', payload)
-  })
-
+  pipe(agent, socket)
 })
-
 
 process.on('uncaughtException', handleFatalError)
 process.on('unhandledRejection', handleFatalError)
